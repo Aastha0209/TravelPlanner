285447cbc268dedd5b587897648b228c786953a4
Aastha Pandey <aasthapandey00209@gmail.com>
2025-10-29 17:45:15 +0530

    Final project update: improved README, added Documentation_Screenshots folder, and finalized TravelPlanner features

--- obj/Debug/net8.0/TravelPlanner.AssemblyInfo.cs
+++ obj/Debug/net8.0/TravelPlanner.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TravelPlanner")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b01a641372625fa2a36103961155e39b1407d615")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89dbd56080b328282702342c4dcda521abdad442")]
 [assembly: System.Reflection.AssemblyProductAttribute("TravelPlanner")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TravelPlanner")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
